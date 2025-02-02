878b6630ef39095b625b08c4a0cafb6608cbb213
Polina <fokichevap@gmail.com>
2025-02-02 21:59:36 +0000

    init nextjs typescript scss prettier eslint

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Geist, Spline_Sans } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
+});
+
+const splineSans = Spline_Sans({
+	variable: "--font-spline-sans",
+	subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+	title: "Nucleus by AM",
+	description: "Articulate Marketing's Nucleus landing page, created for an interview.",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body className={`${geistSans.variable} ${splineSans.variable}`}>{children}</body>
+		</html>
+	);
+}
