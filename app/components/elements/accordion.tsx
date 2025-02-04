12fb7f7f91c1698f8c071a1de26caa087cdfcdd1
Polina <fokichevap@gmail.com>
2025-02-04 02:49:21 +0000

    accordion animation fixes
    - changed animation to rotate just the plus symbol but not the square
    - fixed height issue with accordion items (hacky)
    - removed unused scss files

--- app/components/elements/accordion.tsx
+++ app/components/elements/accordion.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { createContext, useContext, useState } from "react";
+import { useState } from "react";
 import styles from "./accordion.module.scss";
 
 interface AccordionProps {
@@ -9,15 +9,23 @@ interface AccordionProps {
 	}[];
 }
 
-function IconPlus() {
+function IconPlus({ active }: { active: boolean }) {
 	return (
-		<svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+		<svg width="24" height="24" viewBox="0 0 24 24" fill="none" className={styles["accordion__item__trigger__icon"]}>
 			<path
-				d="M8 12H16M12 8V16M5 3H19C20.1046 3 21 3.89543 21 5V19C21 20.1046 20.1046 21 19 21H5C3.89543 21 3 20.1046 3 19V5C3 3.89543 3.89543 3 5 3Z"
+				d="M19 3H5C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21H19C20.1046 21 21 20.1046 21 19V5C21 3.89543 20.1046 3 19 3Z"
 				stroke="currentColor"
 				strokeWidth="2"
 				strokeLinecap="round"
 				strokeLinejoin="round"
+			/>
+			<path
+				d="M8 12H16M12 8V16"
+				stroke="currentColor"
+				strokeWidth="2"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				className={`${styles["accordion__item__trigger__icon__plus"]} ${active ? styles["accordion__item__trigger__icon__plus--rotate"] : ""}`}
 			/>
 		</svg>
 	);
@@ -32,9 +40,7 @@ export default function Accordion({ items }: AccordionProps) {
 				<div className={styles["accordion__item"]} key={index}>
 					<button className={styles["accordion__item__trigger"]} onClick={() => toggleActive(index)} aria-expanded={active === index} type="button">
 						{item.title}
-						<div className={styles[`accordion__item__trigger__icon${active === index ? "--rotate" : ""}`]}>
-							<IconPlus />
-						</div>
+						<IconPlus active={active === index} />
 					</button>
 					<div className={styles[`accordion__item__content${active === index ? "--active" : ""}`]}>{item.content}</div>
 				</div>
